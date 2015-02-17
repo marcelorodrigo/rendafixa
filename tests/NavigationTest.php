aacd83a7e1eb3b7928533c2f7bea74986101ed80
Marcelo Rodrigo <mrodrigow@gmail.com>
2015-02-17 00:53:55 -0200

    Navigation Tests

--- tests/NavigationTest.php
+++ tests/NavigationTest.php
@@ -14,34 +14,4 @@ class NavigationTest extends TestCase {
 		$this->assertEquals(200, $response->getStatusCode());
 	}
 
-	public function testIndicadorSelic()
-	{
-		$response = $this->call('GET', '/indicador/selic');
-		$this->assertEquals(200, $response->getStatusCode());
-	}
-
-	public function testIndicadorIPCS()
-	{
-		$response = $this->call('GET', '/indicador/ipca');
-		$this->assertEquals(200, $response->getStatusCode());
-	}
-
-	public function testIndicadorIGPM()
-	{
-		$response = $this->call('GET', '/indicador/igpm');
-		$this->assertEquals(200, $response->getStatusCode());
-	}
-
-	public function testIndicadorTR()
-	{
-		$response = $this->call('GET', '/indicador/tr');
-		$this->assertEquals(200, $response->getStatusCode());
-	}
-
-	public function testIndicadorPoupanca()
-	{
-		$response = $this->call('GET', '/indicador/poupanca');
-		$this->assertEquals(200, $response->getStatusCode());
-	}
-
 }
