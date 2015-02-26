07059fd1c7b2289e82e99fd9ffc367ec4a3c198d
Marcelo Rodrigo <mrodrigow@gmail.com>
2015-02-26 19:16:28 -0300

    i18n

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,32 +1,32 @@
 @extends('app')
 @section('content')
-    <h2>Simulador de Investimentos</h2>
+    <h2>{{ trans('simulador.title') }}</h2>
     <div class="col-md-4">
         <form>
             <input type="hidden" name="poupanca" id="poupanca" value="{{ $poupanca }}" />
             <div class="form-group">
-                <label for="amount">Valor da Aplicação</label>
+                <label for="amount">{{ trans('simulador.valor.da.aplicacao') }}</label>
                 <div class="input-group">
                     <div class="input-group-addon">R$</div>
-                    <input type="number" class="form-control" id="amount" placeholder="Valor da Aplicação" value="1000"
+                    <input type="number" class="form-control" id="amount" placeholder="{{ trans('simulador.valor.da.aplicacao') }}" value="1000"
                            min="1"/>
                     <div class="input-group-addon">.00</div>
                 </div>
             </div>
             <div class="form-group">
-                <label for="period">Período</label>
+                <label for="period">{{ trans('simulador.periodo') }}</label>
                 <div class="input-group">
-                    <input type="number" class="form-control" id="period" placeholder="Período" value="12" min="1"
+                    <input type="number" class="form-control" id="period" placeholder="{{ trans('simulador.periodo') }}" value="12" min="1"
                            max="1200"/>
-                    <div class="input-group-addon">meses</div>
+                    <div class="input-group-addon">{{ trans('simulador.meses') }}</div>
                 </div>
             </div>
             <div class="form-group">
-                <label for="di">Taxa DI <a href="http://www.cetip.com.br" target="_blank">?</a></label>
+                <label for="di">{{ trans('simulador.taxa.di') }} <a href="http://www.cetip.com.br" target="_blank">?</a></label>
                 <div class="input-group">
-                    <input type="number" class="form-control" id="di" placeholder="Taxa DI" value="{{$cdi}}" min="0"
+                    <input type="number" class="form-control" id="di" placeholder="{{ trans('simulador.taxa.di') }}" value="{{$cdi}}" min="0"
                            max="100"/>
-                    <div class="input-group-addon">% ao ano</div>
+                    <div class="input-group-addon">% {{trans('simulador.ao.ano')}}</div>
                 </div>
             </div>
             <!--
@@ -40,30 +40,30 @@
             </div>
             -->
             <div class="form-group">
-                <label for="cdb">CDB</label>
+                <label for="cdb">{{ trans('simulador.cdb') }}</label>
                 <div class="input-group">
-                    <input type="number" class="form-control" id="cdb" placeholder="Rendimento CDB" value="83" min="0"
+                    <input type="number" class="form-control" id="cdb" placeholder="{{ trans('rendimento.cdb') }}" value="83" min="0"
                            max="100"/>
-                    <div class="input-group-addon">% DI</div>
+                    <div class="input-group-addon">% {{ trans('simulador.di') }}</div>
                 </div>
             </div>
             <div class="form-group">
-                <label for="lci">LCI/LCA</label>
+                <label for="lci">{{ trans('simulador.lci.lca') }}</label>
                 <div class="input-group">
-                    <input type="number" class="form-control" id="lci" placeholder="Rendimento LCI/LCA" value="91"
+                    <input type="number" class="form-control" id="lci" placeholder="{{ trans('rendimento.lci.lca') }}" value="91"
                            min="0" max="100"/>
-                    <div class="input-group-addon">% DI</div>
+                    <div class="input-group-addon">% {{ trans('simulador.di') }}</div>
                 </div>
             </div>
         </form>
     </div>
     <div class="col-md-offset-1 col-md-6">
         <div id="results">
-            <h3>Rentabilidade</h3>
-            <p>Simulação da rentabilidade do seu investimento conforme o tipo de aplicação:</p>
+            <h3>{{ trans('simulador.resultado.titulo') }}</h3>
+            <p>{{ trans('simulador.resultado.descricao') }}</p>
             <hr />
             <div id="result-poupanca">
-                <h4>Poupança</h4>
+                <h4>{{ trans('simulador.poupanca') }}</h4>
                 Valor Líquido: R$ <span class="liquido">0</span><br />
                 <div class="progress">
                     <div id="bar-poupanca" class="progress-bar progress-bar-danger" role="progressbar"
