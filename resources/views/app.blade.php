f461de429469563ae692073c18eb02373017ff21
marcelo.rodrigo <marcelo.rodrigo@fluig.com>
2015-03-03 15:06:04 -0300

    Google Analytics

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -45,6 +45,16 @@
             @yield('content')
         </div>
     </div>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-60343512-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
     <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <script type="text/javascript" src="/js/bootstrap.min.js"></script>
     @yield('footer')
