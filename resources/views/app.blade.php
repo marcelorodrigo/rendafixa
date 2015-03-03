d0cf0b54eba9e91d7d3df6de1025b18ec136b040
marcelo.rodrigo <marcelo.rodrigo@fluig.com>
2015-03-03 15:12:31 -0300

    Google Analytics

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,6 +11,16 @@
     <script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-60343512-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
 </head>
 <body role="document">
     <nav class="navbar navbar-default navbar-fixed-top">
@@ -45,16 +55,6 @@
             @yield('content')
         </div>
     </div>
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-      ga('create', 'UA-60343512-1', 'auto');
-      ga('send', 'pageview');
-
-    </script>
     <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <script type="text/javascript" src="/js/bootstrap.min.js"></script>
     @yield('footer')
