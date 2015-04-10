34b89e73a915cd85a47b9a267b85da3e2214c226
Marcelo Rodrigo <mrodrigow@gmail.com>
2015-04-10 13:24:42 -0300

    Save preferences on financial simulations

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -1,8 +1,11 @@
 <?php namespace App\Http\Controllers;
 
 use App\Business\IndicadorBusiness;
+use App\Business\PreferencesVO;
+use Illuminate\Support\Facades\Cookie;
 
-class WelcomeController extends Controller {
+class WelcomeController extends Controller
+{
 
 	public function index()
 	{
@@ -12,9 +15,13 @@ class WelcomeController extends Controller {
 		$indicadorPoupanca = new IndicadorBusiness(IndicadorBusiness::$POUPANCA);
 		$poupanca = $indicadorPoupanca->getUltimoIndiceXML()->getValue();
 
-		return view('welcome', compact('cdi', 'poupanca'));
-	}
+		$amount = Cookie::get(PreferencesVO::PREFERENCES_AMOUNT, PreferencesVO::DEFAULT_AMOUNT);
+		$period = Cookie::get(PreferencesVO::PREFERENCES_PERIOD, PreferencesVO::DEFAULT_PERIOD);
+		$taxcdb = Cookie::get(PreferencesVO::PREFERENCES_TAXCDB, PreferencesVO::DEFAULT_TAXCDB);
+		$taxlci = Cookie::get(PreferencesVO::DEFAULT_TAXLCI, PreferencesVO::DEFAULT_TAXLCI);
 
+		return view('welcome', compact('cdi', 'poupanca', 'amount', 'period', 'taxcdb', 'taxlci'));
+	}
 
 
 }
--- public/js/calculadora.js
+++ public/js/calculadora.js
@@ -1,69 +1,97 @@
-function getIndexPoupanca() {
-    return parseFloat($("#poupanca").val());
-}
-
-function getIndexCDB() {
-    return ($("#cdb").val() / 100) * getIndexDI() / 12;
-}
-
-function getIndexLCI() {
-    return ($("#lci").val() / 100) * getIndexDI() / 12;
-}
-
-function getIndexDI() {
-    return $("#di").val();
-}
-
-function getIndexIR() {
-    var periodo = $("#period").val();
-    if (periodo <= 3) {
-        return 22.5;
-    } else if (periodo <= 12) {
-        return 20;
-    } else if (periodo <= 24) {
-        return 17.5;
-    } else {
-        return 15;
+$().ready(function () {
+    function savePreferences() {
+        var preferences = {
+            amount: $("#amount").val(),
+            period: $("#period").val(),
+            taxcdb: $("#cdb").val(),
+            taxlci: $("#lci").val(),
+            _token: $("input[name='_token']").val()
+        };
+        $.ajax({
+            type: 'POST',
+            url: window.location.protocol + "//" + window.location.host + '/preferences',
+            data: preferences,
+            success: function (data) {
+                console.log(data);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log(errorThrown)
+            },
+            async: true
+        });
     }
-}
 
-function jurosCompostos(valor, taxa, periodo) {
-    return valor * (Math.pow(1 + taxa / 100, periodo)) - valor;
-}
+    function getIndexPoupanca() {
+        return parseFloat($("#poupanca").val());
+    }
 
-function showResults(){
-    var investimento = $("#amount").val();
-    var periodo = $("#period").val();
+    function getIndexCDB() {
+        return ($("#cdb").val() / 100) * getIndexDI() / 12;
+    }
 
-    var result_poupanca = jurosCompostos(investimento, getIndexPoupanca(), periodo);
+    function getIndexLCI() {
+        return ($("#lci").val() / 100) * getIndexDI() / 12;
+    }
 
-    var result_cdb = jurosCompostos(investimento, getIndexCDB(), periodo);
-    var index_ir = getIndexIR();
-    var ir_cdb = result_cdb * (index_ir / 100);
+    function getIndexDI() {
+        return $("#di").val();
+    }
 
-    var result_lci = jurosCompostos(investimento, getIndexLCI(), periodo);
+    function getIndexIR() {
+        var periodo = $("#period").val();
+        if (periodo <= 3) {
+            return 22.5;
+        } else if (periodo <= 12) {
+            return 20;
+        } else if (periodo <= 24) {
+            return 17.5;
+        } else {
+            return 15;
+        }
+    }
 
-    changeBar('bar-poupanca',result_poupanca.toFixed(2));
-    changeBar('bar-cdb',(result_cdb - ir_cdb).toFixed(2));
-    changeBar('bar-lci',result_lci.toFixed(2));
+    function jurosCompostos(valor, taxa, periodo) {
+        return valor * (Math.pow(1 + taxa / 100, periodo)) - valor;
+    }
 
-    $("#result-poupanca").find('span.liquido').html(result_poupanca.toFixed(2));
+    function showResults(save) {
+        var investimento = $("#amount").val();
+        var periodo = $("#period").val();
 
-    $("#result-cdb").find('span.total').html(result_cdb.toFixed(2));
-    $("#result-cdb").find('span.ir').html(ir_cdb.toFixed(2) + "  <span class='badge'>" + index_ir + "%</span>");
-    $("#result-cdb").find('span.liquido').html((result_cdb - ir_cdb).toFixed(2));
+        var result_poupanca = jurosCompostos(investimento, getIndexPoupanca(), periodo);
 
-    $("#result-lci").find('span.liquido').html(result_lci.toFixed(2));
-}
+        var result_cdb = jurosCompostos(investimento, getIndexCDB(), periodo);
+        var index_ir = getIndexIR();
+        var ir_cdb = result_cdb * (index_ir / 100);
 
-function changeBar(bar, value){
-    var $bar = $('#' + bar);
-    var percent = (value / $("#amount").val() * 100).toFixed(1);
-    $bar.width(percent * 300 / $("#period").val());
-    $bar.html(percent + '%');
-}
-showResults();
+        var result_lci = jurosCompostos(investimento, getIndexLCI(), periodo);
 
-$(".form-control").on('blur',function(){
-    showResults();
-})
+        changeBar('bar-poupanca', result_poupanca.toFixed(2));
+        changeBar('bar-cdb', (result_cdb - ir_cdb).toFixed(2));
+        changeBar('bar-lci', result_lci.toFixed(2));
+
+        $("#result-poupanca").find('span.liquido').html(result_poupanca.toFixed(2));
+
+        $("#result-cdb").find('span.total').html(result_cdb.toFixed(2));
+        $("#result-cdb").find('span.ir').html(ir_cdb.toFixed(2) + "  <span class='badge'>" + index_ir + "%</span>");
+        $("#result-cdb").find('span.liquido').html((result_cdb - ir_cdb).toFixed(2));
+
+        $("#result-lci").find('span.liquido').html(result_lci.toFixed(2));
+        if (save) {
+            savePreferences();
+        }
+    }
+
+    function changeBar(bar, value) {
+        var $bar = $('#' + bar);
+        var percent = (value / $("#amount").val() * 100).toFixed(1);
+        $bar.width(percent * 300 / $("#period").val());
+        $bar.html(percent + '%');
+    }
+
+    showResults(false);
+
+    $(".form-control").on('blur', function () {
+        showResults(true);
+    })
+});
