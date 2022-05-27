e6868cd2214ba599643fe8046a91e31cdef0fce2
Marcelo Wiebbelling <mrodrigow@gmail.com>
2022-05-27 17:51:15 +0200

    Configuring CORS

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['localhost','rendafixa.herokuapp.com'],
 
     'allowed_origins_patterns' => [],
 
