805d134b1f182e30fd51affe7b892e43cc0d77b8
Marcelo Wiebbelling <mrodrigow@gmail.com>
2022-05-27 17:54:08 +0200

    Making sure not using resource integrity feature is safe here.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,7 +15,7 @@
       gtag('js', new Date());
       gtag('config', 'UA-60343512-1');
     </script>
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js" integrity="sha512-DUC8yqWf7ez3JD1jszxCWSVB0DMP78eOyBpMa5aJki1bIRARykviOuImIczkxlj1KhVSyS16w2FSQetkD4UU2w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </head>
 <body role="document">
     <nav class="navbar navbar-default navbar-fixed-top">
